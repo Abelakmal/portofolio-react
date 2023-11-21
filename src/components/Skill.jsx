8661b2627676a183ebcfbac5e07f98e410005c17
Abelakmal <abelakmal06@gmail.com>
2023-11-21 08:52:42 +0700

    edit skill

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,48 +1,61 @@
-import React, { useState } from 'react';
+import next from '../assets/next-js.png';
+import tailwind from '../assets/tailwind-css.png';
+import spring from '../assets/spring.png';
+import express from '../assets/express-js.png';
+import postgresql from '../assets/postgresql.png';
+import java from '../assets/java.png';
+import javascript from '../assets/javascript.png';
+import php from '../assets/php.png';
 
 export const Skill = () => {
-  const [option, setOption] = useState(0);
   return (
-    <section id="skill" className="lg:p-12 max-sm:mt-24 max-lg:mt-24">
-      <h1 className="font-bold pb-2 max-sm:pt-6 max-lg:pl-6 max-lg:pr-6 max-sm:p-2 max-sm:sticky max-sm:top-0  max-sm:pl-4 max-sm:bg-gradient-to-r from-[#000428] to-[#072f52]">SKILL</h1>
-      <div className="judul flex justify-evenly max-lg:ml-6 max-lg:mr-6 bg-black max-sm:ml-4 max-sm:mr-4 text-white lg:p-4 max-sm:py-2 max-lg:py-4 rounded-t-md lg:text-xl lg:font-bold">
-        <span className={option === 0 ? 'active' : ''} onClick={() => setOption(0)}>
-          Front-end
-        </span>
-        <span className={option === 1 ? 'active' : ''} onClick={() => setOption(1)}>
-          Back-end
-        </span>
-        <span className={option === 2 ? 'active' : ''} onClick={() => setOption(2)}>
-          Language programming
-        </span>
+    <section id="skill">
+      <h1 className="text-center p-2 font-bold">MY SKILL</h1>
+      <div className="grid gap-4">
+        <div className="bg-[rgb(15,25,49)] h-96 rounded ">
+          <h2 className="text-center mb-12">Front-end</h2>
+          <div className="flex items-center flex-col ">
+            <i class="fa-brands fa-react text-[#09bfec] text-3xl mb-6"> React</i>
+            <i className="flex mb-6">
+              <img src={next} alt="next-js logo" className="bg-white rounded-full h-12" /> <span className="text-3xl pt-1 pl-2">Next Js</span>
+            </i>
+            <i className="flex mb-6">
+              <img src={tailwind} alt="tailwind logo" className=" h-10" />
+              <span className="text-3xl pt-1 pl-2">Tailwind</span>
+            </i>
+          </div>
+        </div>
+        <div className="bg-[rgb(15,25,49)] h-96 rounded">
+          <h2 className="text-center mb-12">Back-End</h2>
+          <div className="flex items-center flex-col ">
+            <i className="flex mb-6">
+              <img src={spring} alt="next-js logo" className="bg-white rounded-full h-12" /> <span className="text-3xl pt-1 pl-2">Next Js</span>
+            </i>
+            <i className="flex mb-6">
+              <img src={express} alt="next-js logo" className=" h-9" /> <span className="text-3xl pt-1 pl-2">Express Js</span>
+            </i>
+            <i className="flex mb-6">
+              <img src={postgresql} alt="tailwind logo" className=" h-12" />
+              <span className="text-3xl pt-1 pl-2">Postgresql</span>
+            </i>
+          </div>
+        </div>
+        <div className="bg-[rgb(15,25,49)] h-96 rounded col-span-2 ">
+          <h2 className="text-center mb-12">Front-end</h2>
+          <div className="flex items-center flex-col ">
+            <i className="flex mb-6">
+              <img src={java} alt="next-js logo" className="h-12" /> <span className="text-3xl pt-1 pl-2">Next Js</span>
+            </i>
+            <i className="flex mb-6">
+              <img src={javascript} alt="next-js logo" className=" h-9" /> <span className="text-3xl pt-1 pl-2">Javascript</span>
+            </i>
+            <i className="flex mb-6">
+              <img src={php} alt="tailwind logo" className=" h-12" />
+              <span className="text-3xl pt-1 pl-2">PHP</span>
+            </i>
+          </div>
+        </div>
       </div>
-      {option === 0 && (
-        <div className="teknologi flex justify-center items-center h-96 bg-[rgb(10,17,44)] max-lg:ml-6 max-lg:mr-6 rounded-b-xl max-sm:ml-4 max-sm:mr-4">
-          <ul className="flex flex-col text-lg font-medium text-[rgb(85,198,169)]">
-            <li className="p-2">React js</li>
-            <li className="p-2">Next js</li>
-            <li className="p-2">tailwind</li>
-          </ul>
-        </div>
-      )}
-      {option === 1 && (
-        <div className="teknologi flex justify-center items-center h-96 bg-[rgb(10,17,44)] max-lg:ml-6 max-lg:mr-6 max-sm:ml-4 max-sm:mr-4 rounded-b-xl">
-          <ul className="flex flex-col text-lg font-medium text-[rgb(85,198,169)]">
-            <li className="p-2">SpringBoot</li>
-            <li className="p-2">Express</li>
-            <li className="p-2">Postgresql</li>
-          </ul>
-        </div>
-      )}
-      {option === 2 && (
-        <div className="teknologi flex justify-center items-center h-96 bg-[rgb(10,17,44)] max-lg:ml-6 max-lg:mr-6 max-sm:ml-4 max-sm:mr-4 rounded-b-xl">
-          <ul className="flex flex-col text-lg font-medium  text-[rgb(85,198,169)]">
-            <li className="p-2">Java</li>
-            <li className="p-2">Javascript</li>
-            <li className="p-2">Sql</li>
-          </ul>
-        </div>
-      )}
     </section>
   );
 };
